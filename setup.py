9feb2257271a31952f6391f5cd8f3c3b1df9ac1f
IKARUSops <sharifmd.omarfaruk@gmail.com>
2026-05-11 07:52:37 +0600

    commit .

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ REPO_NAME = "Text-Summarizer-Project"
 AUTHOR_USER_NAME = "IKARUSops"
 SRC_REPO = "textsummarizer"
 AUTHOR_EMAIL = "sharif.md.omar.faruq1@gmail.com"
+PACKAGE_ROOT = "src/textsummarizer/src"
 
 
 setuptools.setup(
@@ -19,9 +20,11 @@ setuptools.setup(
     author_email=AUTHOR_EMAIL,
     description="A small python package for NLP app",
     long_description=long_description,
-    long_description_content="text/markdown",
+    long_description_content_type="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
         "Bug Tracker" : f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues"
-    }
+    },
+    package_dir={"": PACKAGE_ROOT},
+    packages=setuptools.find_packages(where=PACKAGE_ROOT)
 )
